965bceb2019de6a40824bc8426701ce10ba44465
elrico-EP <ricogonzalezelena@gmail.com>
2026-01-23 14:44:15 +0100

    Add files via upload

---
+++ firebase-config/config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiQshHBNP3pvV_6wXPf1DNSmXlfCzsGas",
+  authDomain: "zenova-2bf70.firebaseapp.com",
+  projectId: "zenova-2bf70",
+  storageBucket: "zenova-2bf70.firebasestorage.app",
+  messagingSenderId: "500621637060",
+  appId: "1:500621637060:web:f63f3818d3304d6d852091"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
